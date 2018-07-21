fbbbfe92aa288730fdde56511c6d57a9ff456d93
Trevor Jameson <tjameson15@gmail.com>
2018-07-21 15:17:56 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,26 +5,18 @@ def key_for_min_value(name_hash)
   if name_hash == nil || name_hash.size == 0
     return nil
   end
-  # Initialize smallest to {}
   smallest = {}
-
-  # Intialize saved_key
   saved_key = 1000000000000
-  # For each pair in hash
-
   name_hash.each do |key, value|
 
-  # If smallest is {}, smallest equals key/value pair
   if smallest == {}
     smallest = {key => value}
 
-  # If pair[value] is less than smallest[saved_key], smallest equals pair and saved_key equals pair[key]
   elsif value < smallest[saved_key]
     smallest = {key => value}
     saved_key = key
   end
 
-  # Return smallest[saved_key] after loop execution
   saved_key
 end
 
