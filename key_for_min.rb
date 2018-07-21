c45d77862dfb780e2bf0a63f13af92a0e24cb4bf
Trevor Jameson <tjameson15@gmail.com>
2018-07-21 14:44:53 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,19 +5,10 @@ def key_for_min_value(name_hash)
   if name_hash == nil || name_hash.size == 0
     return nil
   end
-  small_value = 0
-  winner = nil
+  smallest = Float::INFINITY
+  all_values = []
+  name_hash.each_key { |key| all_values << key.to_s }
 
-  name_hash.collect do |key, value|
-    if small_value == 0
-      small_value = value
-      winner = key
-    elsif small_value > value
-      small_value = value
-      winner = key
-    end
-  end
-  return name_hash[winner]
 end
 
 # Test data:
