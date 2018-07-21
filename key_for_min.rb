0abf89becba662bc16766c808835895953d5ea6c
Trevor Jameson <tjameson15@gmail.com>
2018-07-21 14:52:20 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,16 +5,7 @@ def key_for_min_value(name_hash)
   if name_hash == nil || name_hash.size == 0
     return nil
   end
-  smallest = 10000000
-  all_values = []
-  name_hash.each_key { |key| all_values << key.to_s }
-  all_values.each do |value|
-    if value.size < smallest.size
-      smallest = value
-    end
-    smallest
-  end
-  all_values.index_of(smallest)
+  
 end
 
 # Test data:
