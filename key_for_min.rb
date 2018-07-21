655a4c15a467f682e24b38015f3d9845e4481bb2
Trevor Jameson <tjameson15@gmail.com>
2018-07-21 15:18:17 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,18 +5,17 @@ def key_for_min_value(name_hash)
   if name_hash == nil || name_hash.size == 0
     return nil
   end
+  
   smallest = {}
   saved_key = 1000000000000
+  
   name_hash.each do |key, value|
-
   if smallest == {}
     smallest = {key => value}
-
   elsif value < smallest[saved_key]
     smallest = {key => value}
     saved_key = key
   end
-
   saved_key
 end
 
